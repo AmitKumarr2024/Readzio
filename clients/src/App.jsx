3ab7eefec9c1f04a6cd6adac51c088917824e35f
Amit kumar <codes.amitkumar@gmail.com>
2025-05-17 21:58:02 +0530

    setup of file

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import toast from 'react-hot-toast'
 
 function App() {
   const [count, setCount] = useState(0)
+  toast.success("Hello Amit")
 
   return (
     <>
       <div>
+        
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
