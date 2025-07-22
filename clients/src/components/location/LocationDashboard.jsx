0fc1eb21a0ccbb13b4732ed3595d8abab4bc5875
Amit kumar <codes.amitkumar@gmail.com>
2025-07-22 22:16:28 +0530

    location zoom

--- clients/src/components/location/LocationDashboard.jsx
+++ clients/src/components/location/LocationDashboard.jsx
@@ -418,7 +418,7 @@ const LocationDashboard = () => {
                 backgroundColor: "transparent",
               }}
               className="rounded-lg z-0"
-              maxZoom={10}
+              maxZoom={6}
             >
               <TileLayer
                 url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
