e1c069ef80a9e1ecc45ca7698af4bbec79352b0f
Amit kumar <codes.amitkumar@gmail.com>
2025-07-25 00:03:59 +0530

    mail layout thing improve try 25

--- servers/config/dailyPostEmailTemplate.js
+++ servers/config/dailyPostEmailTemplate.js
@@ -190,10 +190,10 @@ export const DAILY_POST_EMAIL_TEMPLATE = `<!DOCTYPE html>
                  <tr>
                 {{#if this.author.avatar}}
                 <td style="vertical-align: middle; padding-right: 8px;">
-                  <img src="{{this.author.avatar}}" alt="{{this.author.name}}" width="28" height="28" style="border-radius: 50%; display: block;" />
+                  <img src="{{this.author.avatar}}" alt="{{this.author.name}}" width="30" height="30" style="border-radius: 50%; display: block;" />
                 </td>
                 {{/if}}
-                <td style="vertical-align: middle; font-size: 14px; color: #6b7280;">
+                <td style="vertical-align: middle; font-size: 18px; color: #6b7280;">
                   by {{this.author.name}}
                 </td>
               </tr>
@@ -202,22 +202,22 @@ export const DAILY_POST_EMAIL_TEMPLATE = `<!DOCTYPE html>
 
          <table cellpadding="0" cellspacing="0" role="presentation" width="100%" style="margin: 16px 0; height: 60px;">
             <tr>
-               <td style="font-size: 55px; font-weight: bold; color: #374151; padding-right: 20px;">
+               <td style="font-size: 65px; font-weight: bold; color: #374151; padding-right: 20px;">
                 📖
               </td>
-              <td width="16.6%" align="left" valign="middle" style="font-size: 18px; font-weight: bold; line-height: 24px; color: #374151;">
+              <td width="16.6%" align="left" valign="middle" style="font-size: 15px; font-weight: bold; line-height: 24px; color: #6b7280;">
                 {{#if this.readTime}}{{this.readTime}}{{else}}0 min{{/if}}
               </td>
-               <td style="font-size: 55px; font-weight: bold; color: #374151; padding-right: 20px;">
+               <td style="font-size: 65px; font-weight: bold; color: #6b7280; padding-right: 20px;">
                 ❤️
               </td>
-              <td width="16.6%" align="left" valign="middle" style="font-size: 18px; font-weight: bold; line-height: 24px; color: #374151;">
+              <td width="16.6%" align="left" valign="middle" style="font-size: 15px; font-weight: bold; line-height: 24px; color: #6b7280;">
                 {{#if this.likesCount}}{{this.likesCount}}{{else}}0{{/if}}
               </td>
-               <td style="font-size: 55px; font-weight: bold; color: #374151; padding-right: 20px;">
+               <td style="font-size: 65px; font-weight: bold; color: #6b7280; padding-right: 20px;">
                 💬
               </td>
-              <td width="16.6%" align="left" valign="middle" style="font-size: 18px; font-weight: bold; line-height: 24px; color: #374151;">
+              <td width="16.6%" align="left" valign="middle" style="font-size: 15px; font-weight: bold; line-height: 24px; color: #6b7280;">
                 {{#if this.commentsCount}}{{this.commentsCount}}{{else}}0{{/if}}
               </td>
             </tr>
