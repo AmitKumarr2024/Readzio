2ac71536005b925d37acdc1f9c4f22070b02b711
Amit kumar <codes.amitkumar@gmail.com>
2025-07-24 22:18:06 +0530

    mail layout thing improve try 12

--- servers/config/dailyPostEmailTemplate.js
+++ servers/config/dailyPostEmailTemplate.js
@@ -200,21 +200,21 @@ export const DAILY_POST_EMAIL_TEMPLATE = `<!DOCTYPE html>
               </table>
 
 
-              <table cellpadding="0" cellspacing="0" role="presentation" width="100%" height="40%" style="margin: 16px 0;">
+             <table cellpadding="0" cellspacing="0" role="presentation" width="100%" style="margin: 16px 0;">
                 <tr>
-                  <td width="16.6%" align="center" style="font-size: 18px; font-weight: bold; color: #374151;">
+                  <td width="16.6%" align="center" style="font-size: 26px; line-height: 1.2; font-weight: bold; color: #374151;">
                     📖
                   </td>
                   <td width="16.6%" align="left" style="font-size: 18px; font-weight: bold; color: #374151;">
                     {{#if this.readTime}}{{this.readTime}}{{else}}0 min{{/if}}
                   </td>
-                  <td width="16.6%" align="center" style="font-size: 18px; font-weight: bold; color: #374151;">
+                  <td width="16.6%" align="center" style="font-size: 26px; line-height: 1.2; font-weight: bold; color: #374151;">
                     ❤️
                   </td>
                   <td width="16.6%" align="left" style="font-size: 18px; font-weight: bold; color: #374151;">
                     {{#if this.likesCount}}{{this.likesCount}}{{else}}0{{/if}}
                   </td>
-                  <td width="16.6%" align="center" style="font-size: 18px; font-weight: bold; color: #374151;">
+                  <td width="16.6%" align="center" style="font-size: 26px; line-height: 1.2; font-weight: bold; color: #374151;">
                     💬
                   </td>
                   <td width="16.6%" align="left" style="font-size: 18px; font-weight: bold; color: #374151;">
@@ -222,6 +222,7 @@ export const DAILY_POST_EMAIL_TEMPLATE = `<!DOCTYPE html>
                   </td>
                 </tr>
               </table>
+
 
 
 
