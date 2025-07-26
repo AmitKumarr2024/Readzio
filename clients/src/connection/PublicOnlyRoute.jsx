642ca1981c36ceb6e3f0d31b36b2c5992eb90ea9
Amit kumar <codes.amitkumar@gmail.com>
2025-07-26 22:12:35 +0530

    fix router login user can not go again back to login 4

--- clients/src/connection/PublicOnlyRoute.jsx
+++ clients/src/connection/PublicOnlyRoute.jsx
@@ -1,20 +1,33 @@
-import { useSelector } from "react-redux";
+// src/routes/PublicOnlyRoute.jsx
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
+import { checkAuth } from "../store/authSlice";
 
 const PublicOnlyRoute = ({ children }) => {
-  const { isAuthenticated, authInitialized } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
   const location = useLocation();
+  const { isAuthenticated, authInitialized, isCheckingAuth } = useSelector(
+    (state) => state.auth
+  );
 
-  // 1. Wait for checkAuth to finish
+  console.log("isAuthenticated:", isAuthenticated);
+  console.log("authInitialized:", authInitialized);
+  console.log("location.state:", location.state);
+
+  useEffect(() => {
+    if (!authInitialized && !isCheckingAuth) {
+      dispatch(checkAuth());
+    }
+  }, [authInitialized, isCheckingAuth, dispatch]);
+
   if (!authInitialized) return null;
 
-  // 2. Redirect if already logged in
   if (isAuthenticated) {
-    const from = location?.state?.from?.pathname || "/";
+    const from = location.state?.from?.pathname || "/";
     return <Navigate to={from} replace />;
   }
 
-  // 3. Allow access for guests
   return children;
 };
 
