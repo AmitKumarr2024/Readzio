592610301ae1e8632ebc87f1cb2b22c9e0de0f1a
Amit kumar <codes.amitkumar@gmail.com>
2025-08-01 18:14:18 +0530

    text 14

--- clients/src/components/actualPostDisplay/TextBlock.jsx
+++ clients/src/components/actualPostDisplay/TextBlock.jsx
@@ -1,13 +1,9 @@
 const TextBlock = ({ value }) => (
   <div
-    className="
-      prose prose-sm sm:prose-base dark:prose-invert max-w-none
-      text-text-main-light dark:text-text-main-dark
-      [&>ul]:list-disc [&>ul]:pl-5
-      [&>ol]:list-decimal [&>ol]:pl-5
-      [&>li]:my-1
-    "
-    dangerouslySetInnerHTML={{ __html: value || "<p>&nbsp;</p>" }}
+    className="rich-content my-4"
+    dangerouslySetInnerHTML={{
+      __html: value || "<p>&nbsp;</p>",
+    }}
   />
 );
 
