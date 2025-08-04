ebb94af14539c8347e5f160f3cc0893af16ed70e
Amit kumar <codes.amitkumar@gmail.com>
2025-08-04 13:57:42 +0530

    render issue solve 12

--- servers/config/mongodb.js
+++ servers/config/mongodb.js
@@ -19,8 +19,6 @@ const connectDb = async () => {
 
   try {
     await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 10000,
       connectTimeoutMS: 10000,
     });
