b203b6a93f674238f5739683ed5c86de638404c5
Amit kumar <codes.amitkumar@gmail.com>
2025-08-20 12:25:13 +0530

    some thing fix2

--- clients/src/AppRootFile/components/CookieConsentBanner.jsx
+++ clients/src/AppRootFile/components/CookieConsentBanner.jsx
@@ -1,6 +1,6 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { useDispatch, useEffect } from "react";
+import { useEffect } from "react";
 import { saveUserConsent } from "../../store/userSlice";
 
 export default function CookieConsentBar() {
