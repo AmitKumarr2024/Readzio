534fda4bb2b904d60aa0319569f36bfcd9355ec2
Amit kumar <codes.amitkumar@gmail.com>
2025-08-22 18:46:09 +0530

    some thing fix 73

--- servers/helpers/securityHeaders.js
+++ servers/helpers/securityHeaders.js
@@ -1,13 +1,18 @@
-// servers/middleware/securityHeaders.js
+// servers/helpers/securityHeaders.js
 export const setSecurityHeaders = (req, res, next) => {
   res.setHeader(
     "Permissions-Policy",
-    "geolocation=(), microphone=(), camera=()"
+    "geolocation=(self), microphone=(), camera=()"
   );
   res.setHeader("X-Frame-Options", "SAMEORIGIN");
   res.setHeader(
     "Content-Security-Policy",
-    "default-src 'self'; img-src 'self' data: https://*.instagram.com https://res.cloudinary.com https://*.google.com https://*.googlesyndication.com; script-src 'self' https://*.instagram.com 'unsafe-inline' https://*.google.com https://*.googlesyndication.com; frame-src 'self' https://*.instagram.com https://*.google.com https://*.googlesyndication.com; style-src 'self' 'unsafe-inline'; connect-src 'self' https://*.instagram.com https://res.cloudinary.com https://*.google.com https://*.googlesyndication.com"
+    "default-src 'self'; " +
+      "img-src 'self' data: https://*.instagram.com https://res.cloudinary.com https://*.google.com https://*.googlesyndication.com https://pagead2.googlesyndication.com; " +
+      "script-src 'self' https://*.instagram.com 'unsafe-inline' https://*.google.com https://*.googlesyndication.com https://pagead2.googlesyndication.com; " +
+      "frame-src 'self' https://*.instagram.com https://*.google.com https://*.googlesyndication.com https://pagead2.googlesyndication.com; " +
+      "style-src 'self' 'unsafe-inline'; " +
+      "connect-src 'self' https://*.instagram.com https://res.cloudinary.com https://*.google.com https://*.googlesyndication.com https://pagead2.googlesyndication.com"
   );
   next();
 };
