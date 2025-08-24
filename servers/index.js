69fc67355e42acf9023b10acabe12dcf3e9554e3
Amit kumar <codes.amitkumar@gmail.com>
2025-08-24 17:42:49 +0530

    all fix 9

--- servers/index.js
+++ servers/index.js
@@ -315,7 +315,7 @@ if (NODE_ENV === "production") {
         },
       })
     );
-    app.get("*", (req, res, next) => {
+    app.get("/{*any}", (req, res, next) => {
       const disallowed = [
         req.path.startsWith("/api"),
         req.path.startsWith("/public"),
