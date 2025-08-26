de982059d4f8cf8d4967b73bf488f5e8fdec7c4d
Amit kumar <codes.amitkumar@gmail.com>
2025-08-26 07:48:11 +0530

    image size 3

--- clients/src/components/PostFeature/PostImageBlock.jsx
+++ clients/src/components/PostFeature/PostImageBlock.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiUpload } from "react-icons/fi";
 import { MdDeleteForever } from "react-icons/md";
 import { motion } from "framer-motion";
