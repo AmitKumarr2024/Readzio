9e9cab71e09887975bce113a545f9d0e52de788d
Amit kumar <codes.amitkumar@gmail.com>
2025-08-28 00:34:07 +0530

    border animation 10

--- clients/src/components/Post/SuggestedPosts.jsx
+++ clients/src/components/Post/SuggestedPosts.jsx
@@ -174,7 +174,7 @@ const SuggestedPosts = ({ postId, className }) => {
           <div
             className={
               className ||
-              "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2 max-w-8xl mx-auto"
+              "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 max-w-8xl mx-auto"
             }
           >
             {displayedPosts.map((post, index) => (
