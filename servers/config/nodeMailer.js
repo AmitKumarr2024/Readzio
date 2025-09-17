57828057ef9f241b1e07229a5be84bb3deb31403
Amit kumar <codes.amitkumar@gmail.com>
2025-09-17 16:33:53 +0530

    fixing thing email log 2

--- servers/config/nodeMailer.js
+++ servers/config/nodeMailer.js
@@ -1,15 +1,16 @@
-import nodemailer from 'nodemailer';
-import { SMTP_PASS, SMTP_USER } from './dotenv.js';
+import nodemailer from "nodemailer";
+import { SMTP_PASS, SMTP_USER } from "./dotenv.js";
 
 if (!SMTP_USER || !SMTP_PASS) {
-  throw new Error('SMTP_USER and SMTP_PASS must be defined');
+  throw new Error("SMTP_USER and SMTP_PASS must be defined");
 }
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  port: 465,
-  secure: true,
-  auth: { user: SMTP_USER, pass: SMTP_PASS },
+  service: "gmail", // tells nodemailer to use Gmail settings internally
+  auth: {
+    user: SMTP_USER, // your full Gmail address
+    pass: SMTP_PASS, // your Gmail App Password (NOT your real password)
+  },
 });
 
 export default transporter;
