2bf14ca3da7017bbb71e73a52d32e0b5347b6090
Amit kumar <codes.amitkumar@gmail.com>
2025-09-21 18:53:34 +0530

    fix email 18

--- servers/config/nodeMailer.js
+++ servers/config/nodeMailer.js
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import { SMTP_PASS, SMTP_USER } from "../";
+import { SMTP_PASS, SMTP_USER } from "../../servers/config/dotenv.js";
 
 // Validate environment variables
 if (!SMTP_USER || !SMTP_PASS) {
