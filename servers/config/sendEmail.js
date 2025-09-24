6093b337ada84d55a077f08c56c72458b1384802
Amit kumar <codes.amitkumar@gmail.com>
2025-09-24 21:21:42 +0530

    try to fix email issue with Resend APi 16

--- servers/config/sendEmail.js
+++ servers/config/sendEmail.js
@@ -1,5 +1,5 @@
 import { Resend } from "resend";
-import { RESEND_API_KEY, SENDER_EMAIL } from "../../servers/config/dotenv.js";
+import { RESEND_API_KEY, SENDER_EMAIL } from "./dotenv";
 
 // Debug logging
 console.log(
