a3656422cd977d800d886665bedaa39e7836b3c0
Amit kumar <codes.amitkumar@gmail.com>
2025-10-05 13:45:49 +0530

    fixing serve route to connect with frontend 5

--- servers/index.js
+++ servers/index.js
@@ -147,6 +147,8 @@ app.use(
         "http://localhost:5173",
         "http://localhost:8001",
         "https://inkshaa.onrender.com",
+        "http://134.209.152.142:3000",
+        
       ].filter(Boolean);
 
       if (!origin) return callback(null, true);
