95b58a7b077a37f70421f8d542b0e93444bb9f08
Amit kumar <codes.amitkumar@gmail.com>
2025-11-01 17:16:33 +0530

    change name from playlist to post list 5

--- clients/src/pages/AuthorProfilePage.jsx
+++ clients/src/pages/AuthorProfilePage.jsx
@@ -31,11 +31,11 @@ import { tabsConfig } from "../config/tabsConfig";
 import { debounce } from "lodash";
 import SubscriptionEligibilityProgress from "../components/Author/SubscriptionEligibilityProgress";
 import LocationDashboard from "../components/location/LocationDashboard";
-import PlaylistList from "../components/Playlist/PlaylistList";
 
 const AboutAuthor = lazy(() => import("../components/Author/AboutAuthor"));
 const AllPosts = lazy(() => import("../components/Author/post/AllPosts"));
 const PinnedPost = lazy(() => import("../components/Author/post/PinnedPost"));
+const PlaylistList = lazy(() => import("../components/Playlist/PlaylistList"));
 const AuthorPolls = lazy(() =>
   import("../components/Author/polls/AuthorPolls")
 );
