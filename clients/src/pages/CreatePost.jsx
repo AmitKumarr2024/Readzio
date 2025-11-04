dfb9914445c8c2cd81389a09ff10b5f7f0a726e7
Amit kumar <codes.amitkumar@gmail.com>
2025-11-04 21:52:36 +0530

    try to fix editor post ui 13

--- clients/src/pages/CreatePost.jsx
+++ clients/src/pages/CreatePost.jsx
@@ -436,8 +436,8 @@ const CreatePost = () => {
 
       {/* 🔹 Show editor only if not submitting */}
       {!showPostTypeModal && !showCategoryModal && !isSubmitting && (
-        <div className="min-w-full pl-10 flex container justify-around items-center flex-col flex-wrap md:flex-row">
-          <div className="w-full flex justify-start px-4 pt-10 pl-11">
+        <div className="min-w-full pl-10 md:pl-72 flex container justify-around items-center flex-col flex-wrap md:flex-row">
+          <div className="w-full flex justify-start px-4 pt-10">
             <button
               onClick={() => {
                 // console.log("[CreatePost] Cancel & Go Back clicked");
