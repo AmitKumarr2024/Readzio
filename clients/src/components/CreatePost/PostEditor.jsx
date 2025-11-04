c46d7c167acd13b79b94361b546d09bf207a68ac
Amit kumar <codes.amitkumar@gmail.com>
2025-11-04 15:12:06 +0530

    try to fix editor post ui 3

--- clients/src/components/CreatePost/PostEditor.jsx
+++ clients/src/components/CreatePost/PostEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { toast } from "react-hot-toast";
 import { v4 as uuidv4 } from "uuid";
