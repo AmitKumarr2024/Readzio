f9154bc3dc1ea9e47d23b644b7b58b027d5177c9
Amit kumar <codes.amitkumar@gmail.com>
2025-11-12 23:30:16 +0530

    fixing playlist type postlist 3

--- clients/src/components/Playlist/PlaylistDetail.jsx
+++ clients/src/components/Playlist/PlaylistDetail.jsx
@@ -278,6 +278,7 @@ const PlaylistDetail = ({ playlistId }) => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {currentPlaylist.posts.map((post, index) => {
             const fullPost = postMap.get(post._id) || post;
+            console.log("fullPost", fullPost);
 
             return (
               <div
